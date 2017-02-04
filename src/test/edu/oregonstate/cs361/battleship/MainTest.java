7bc6e40c92ef70d2a12306cef80f9fe0846f0df2
Liv Vitale <vitalec@oregonstate.edu>
2017-02-03 22:40:15 -0800

    Improved test coverage, added win/lose conditions, added notifications

--- src/test/edu/oregonstate/cs361/battleship/MainTest.java
+++ src/test/edu/oregonstate/cs361/battleship/MainTest.java
@@ -32,27 +32,34 @@ class MainTest {
 
     @Test
     public void testGetModel() {
-        TestResponse res = request("GET", "/model");
+        TestResponse res = request("GET", "/model", null);
         assertEquals(200, res.status);
-        assertEquals("MODEL",res.body);
+        assertEquals("{\"destroyer\":{\"start\":{\"across\":0,\"down\":0},\"end\":{\"across\":0,\"down\":0},\"name\":\"destroyer\",\"length\":2},\"submarine\":{\"start\":{\"across\":0,\"down\":0},\"end\":{\"across\":0,\"down\":0},\"name\":\"submarine\",\"length\":3},\"cruiser\":{\"start\":{\"across\":0,\"down\":0},\"end\":{\"across\":0,\"down\":0},\"name\":\"cruiser\",\"length\":3},\"battleship\":{\"start\":{\"across\":0,\"down\":0},\"end\":{\"across\":0,\"down\":0},\"name\":\"battleship\",\"length\":4},\"aircraftCarrier\":{\"start\":{\"across\":0,\"down\":0},\"end\":{\"across\":0,\"down\":0},\"name\":\"aircraftCarrier\",\"length\":5},\"computerDestroyer\":{\"start\":{\"across\":1,\"down\":1},\"end\":{\"across\":2,\"down\":1},\"name\":\"computerDestroyer\",\"length\":2},\"computerSubmarine\":{\"start\":{\"across\":7,\"down\":3},\"end\":{\"across\":7,\"down\":5},\"name\":\"computerSubmarine\",\"length\":3},\"computerCruiser\":{\"start\":{\"across\":6,\"down\":2},\"end\":{\"across\":6,\"down\":4},\"name\":\"computerCruiser\",\"length\":3},\"computerBattleship\":{\"start\":{\"across\":3,\"down\":4},\"end\":{\"across\":6,\"down\":4},\"name\":\"computerBattleship\",\"length\":4},\"computerAircraftCarrier\":{\"start\":{\"across\":5,\"down\":5},\"end\":{\"across\":5,\"down\":9},\"name\":\"computerAircraftCarrier\",\"length\":5},\"playerHits\":[],\"playerMisses\":[],\"computerHits\":[],\"computerMisses\":[]}",res.body);
     }
 
     @Test
     public void testPlaceShip() {
-        TestResponse res = request("POST", "/placeShip/aircraftCarrier/1/1/horizontal");
+        Gson gson = new Gson();
+        String model = gson.toJson(new BattleshipModel());
+        TestResponse res = request("POST", "/placeShip/aircraftCarrier/1/1/horizontal", model);
+
         assertEquals(200, res.status);
-        assertEquals("SHIP",res.body);
+        assertEquals("{\"destroyer\":{\"start\":{\"across\":0,\"down\":0},\"end\":{\"across\":0,\"down\":0},\"name\":\"destroyer\",\"length\":2},\"submarine\":{\"start\":{\"across\":0,\"down\":0},\"end\":{\"across\":0,\"down\":0},\"name\":\"submarine\",\"length\":3},\"cruiser\":{\"start\":{\"across\":0,\"down\":0},\"end\":{\"across\":0,\"down\":0},\"name\":\"cruiser\",\"length\":3},\"battleship\":{\"start\":{\"across\":0,\"down\":0},\"end\":{\"across\":0,\"down\":0},\"name\":\"battleship\",\"length\":4},\"aircraftCarrier\":{\"start\":{\"across\":1,\"down\":1},\"end\":{\"across\":1,\"down\":5},\"name\":\"aircraftCarrier\",\"length\":5},\"computerDestroyer\":{\"start\":{\"across\":1,\"down\":1},\"end\":{\"across\":2,\"down\":1},\"name\":\"computerDestroyer\",\"length\":2},\"computerSubmarine\":{\"start\":{\"across\":7,\"down\":3},\"end\":{\"across\":7,\"down\":5},\"name\":\"computerSubmarine\",\"length\":3},\"computerCruiser\":{\"start\":{\"across\":6,\"down\":2},\"end\":{\"across\":6,\"down\":4},\"name\":\"computerCruiser\",\"length\":3},\"computerBattleship\":{\"start\":{\"across\":3,\"down\":4},\"end\":{\"across\":6,\"down\":4},\"name\":\"computerBattleship\",\"length\":4},\"computerAircraftCarrier\":{\"start\":{\"across\":5,\"down\":5},\"end\":{\"across\":5,\"down\":9},\"name\":\"computerAircraftCarrier\",\"length\":5},\"playerHits\":[],\"playerMisses\":[],\"computerHits\":[],\"computerMisses\":[]}", res.body);
     }
 
-    private TestResponse request(String method, String path) {
+    private TestResponse request(String method, String path, String body) {
         try {
             URL url = new URL("http://localhost:4567" + path);
             HttpURLConnection connection = (HttpURLConnection) url.openConnection();
             connection.setRequestMethod(method);
             connection.setDoOutput(true);
+            if(body != null) {
+                byte[] bodyBytes = body.getBytes("UTF-8");
+                connection.getOutputStream().write(bodyBytes);
+            }
             connection.connect();
-            String body = IOUtils.toString(connection.getInputStream());
-            return new TestResponse(connection.getResponseCode(), body);
+            String responseBody = IOUtils.toString(connection.getInputStream());
+            return new TestResponse(connection.getResponseCode(), responseBody);
         } catch (IOException e) {
             e.printStackTrace();
             fail("Sending request failed: " + e.getMessage());
